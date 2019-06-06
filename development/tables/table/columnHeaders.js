abe348833032a0f5d4224c2fc2b85c680a7f5a3b
Ida Ebkes <contact@ida-ebkes.eu>
2019-06-06 08:12:24 +0200

    Responsive design

---
+++ development/tables/table/columnHeaders.js
@@ -0,0 +1,40 @@
+setTimeout(function() {
+  alert('tables')
+  //addColumnHeadersPerRow(tables[0])
+}, 2727);
+//alert( parseFloat( window.getComputedStyle(document.body).getPropertyValue('width') )  )
+function addColumnHeadersPerRow(table) {
+// If we have a small device, show column-header above any cell.
+  var style = window.getComputedStyle(table.ele.children[0].children[0])
+  var col = style.getPropertyValue('color')
+  var bgCol = style.getPropertyValue('background-color')
+  style = `
+    li:before {
+      background: black;
+      color: white;
+      display: block;
+    }
+  `
+
+  var headers = localStorage.getItem(table.id).split(rowSeparator)[0].split(cellSeparator)
+  var rowEles = table.ele.children
+  var rowEle = rowEles[0]
+  var cellEles = rowEle.children
+
+  for(var j=0; j < cellEles.length; j++) {
+
+    var header = headers[j]
+
+    if(header == '') header = '[Empty Header]'
+
+    var number = Number(j) + 1
+
+    style += `ul:not(:first-child) li:nth-child(${number}):before {
+      content: "${header}";
+      display: block;
+    }`
+  }
+
+
+  var styleEle = addEle(document.head, style, 'style')
+}
